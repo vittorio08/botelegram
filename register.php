20ac5af4ee49a49b9f0765e01dd57cb89ec04f85
vittorio08 <84317378+vittorio08@users.noreply.github.com>
2021-05-24 11:59:48 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
 		//inserite vostro indirizzo su heroku 
 		//https://username.herokuapp.com/execute.php
 		//inserisci l'ure dell'execute su heroku es: https://telegram2.herokuapp.com/execute.php
-		$WEBHOOK_URL = ' https://bottele01.herokuapp.com/execute.php';
+		$WEBHOOK_URL = 'https://bottele01.herokuapp.com/execute.php';
 		//inserite il vostro token che vi ha fornito l'utente telegram @botfather
 		$BOT_TOKEN = '1740101243:AAGbk44MxIp53038EZhy2Xpi-W2rg6mqAl0';
 
